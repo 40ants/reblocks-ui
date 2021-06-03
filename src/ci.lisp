66f6d5725a86af3cd9a13778e5d8ebd1acf7b883
Alexander Artemenko <svetlyak.40wt@gmail.com>
2021-06-03 15:39:57 +0300

    Added a CI workflow to run linter.

---
+++ src/ci.lisp
@@ -0,0 +1,28 @@
+(defpackage #:weblocks-ui/ci
+  (:use #:cl)
+  (:import-from #:40ants-ci/jobs/linter
+                #:linter)
+  (:import-from #:40ants-ci/jobs/run-tests
+                #:run-tests)
+  (:import-from #:40ants-ci/jobs/docs
+                #:build-docs)
+  (:import-from #:40ants-ci/workflow
+                #:defworkflow))
+(in-package weblocks-ui/ci)
+
+
+;; (defworkflow docs
+;;   :on-push-to "master"
+;;   :by-cron "0 10 * * 1"
+;;   :cache t
+;;   :jobs ((build-docs)))
+
+
+(defworkflow ci
+  :on-push-to "master"
+  :by-cron "0 10 * * 1"
+  :on-pull-request t
+  :cache t
+  :jobs ((linter)
+         ;; (run-tests :coverage t)
+         ))
