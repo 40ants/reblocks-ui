449a945fb4ff121aea68cd77535f577a6212a7ef
Alexander Artemenko <svetlyak.40wt@gmail.com>
2022-02-26 04:37:04 +0300

    Added reblocks-ui/popup:popup-widget.

---
+++ src/popup.lisp
@@ -0,0 +1,94 @@
+(uiop:define-package #:reblocks-ui/popup
+  (:use #:cl)
+  (:import-from #:reblocks/widget
+                #:defwidget)
+  (:import-from #:reblocks-lass)
+  (:export #:visible-p
+           #:popup-widget
+           #:show-popup
+           #:render-popup-content
+           #:hide-popup))
+(in-package #:reblocks-ui/popup)
+
+
+(defwidget popup-widget (reblocks-ui:ui-widget)
+  ((visible :initform nil
+            :accessor visible-p)))
+
+
+(defgeneric show-popup (widget)
+  (:method ((widget popup-widget))
+    (setf (visible-p widget) t)
+    (reblocks/widget:update widget)))
+
+
+(defgeneric hide-popup (widget)
+  (:method ((widget popup-widget))
+    (setf (visible-p widget) nil)
+    (reblocks/widget:update widget)))
+
+
+(defmethod reblocks/widget:get-css-classes ((widget popup-widget))
+  (append (when (visible-p widget)
+            (list "active"))
+          (list* "popup"
+                 (call-next-method))))
+
+
+(defgeneric render-popup-content (widget)
+  (:method ((widget popup-widget))
+    (reblocks/html:with-html
+      (:p (format nil "Define RENDER-POPUP-CONTENT method for ~S class."
+                  (class-name (class-of widget)))))))
+
+
+(defmethod reblocks/widget:render ((widget popup-widget))
+  (reblocks/html:with-html
+    (:div :class "popup-content"
+          (render-popup-content widget))))
+
+
+(defun make-css ()
+  (reblocks-lass:make-dependency
+    '(body
+      (.popup
+       :position fixed
+       :top 0
+       :left 0
+       :width 100%
+       :height 100vh
+       :background "rgba(0,0,0,0.5)"
+       :opacity 0
+       :pointer-events none
+       :transition 0.5s all
+
+       (.popup-content
+        :position absolute
+        :top 50%
+        :left 50%
+        ;; Here we are centering content and
+        ;; shrinking it to a point in the center of the screen
+        :transform "translate(-50%, -50%) scale(0)" 
+        :background "#fff"
+        ;; TODO: probably we need to customize this param
+        ;; or to define it as min-width.
+        :width 400px
+        :padding 25px
+        :transition 0.5s all))
+      ((:and .popup .active)
+       :opacity 1
+       :pointer-events all
+       :transition 0.5s all
+
+       (.popup-content
+        ;; Seems these animations don't work when we
+        ;; to UPDATE widget. Maybe we need to
+        ;; SEND-SCRIPT instead and just to add a class
+        ;; on the frontend, to make everything animated!
+        :transform "translate(-50%, -50%) scale(1)"
+        :transition 0.5s all)))))
+
+
+(defmethod reblocks/dependencies:get-dependencies ((widget popup-widget))
+  (list* (make-css)
+         (call-next-method)))
